05e673fc66eea7c12ff36fe1b83297540ec019cc
ubuntu <ubuntu@localhost.localdomain>
2021-07-05 23:10:07 +0800

    2

--- contest/1539/f/f.cpp
+++ contest/1539/f/f.cpp
@@ -26,6 +26,8 @@
 #define clr(arr) memset(arr, 0, sizeof arr)
 #define bset std::bitset<N>
 #define pi std::pair<int, int>
+#define ls (u << 1)
+#define rs (u << 1 | 1)
 inline ll read ()
 {
     ll x = 0; bool flag = 0; char ch = getchar();
@@ -46,22 +48,113 @@ inline ll qpow (ll x, int y)
     }
     return ret;
 }
-struct edge {
-    int nxt, v;
-}e[N << 1];
 int head[N], tot;
-void addedge (int u, int v)
+int n, m, a[N], b[N], c[N]; 
+int abs (int x) {return x < 0 ? -x : x;}
+struct tree {
+    int sum, lx, rx;
+}t[N << 2];
+void update (int u)
 {
-    e[++tot] = (edge) {head[u], v};
-    head[u] = tot;
+    t[u].sum = t[ls].sum + t[rs].sum;
+    t[u].lx = std::max(t[ls].lx, t[ls].sum + t[rs].lx);
+    t[u].rx = std::max(t[rs].rx, t[rs].sum + t[ls].rx);
 }
-int n, m, a[N]; 
+void build (int u, int l, int r)
+{
+    t[u].lx = t[u].rx = t[u].sum = 0;
+    if (l == r)
+    {
+        t[u].sum = -1;
+        return;
+    }
+    int mid = l + r >> 1;
+    build(ls, l, mid);
+    build(rs, mid + 1, r);
+    update(u);
+}
+void modify (int u, int l, int r, int x, int v)
+{
+    if (l == r)
+    {
+        t[u].sum = v;
+        t[u].lx = t[u].rx = std::max(0, v);
+        return;
+    }
+    int mid = l + r >> 1;
+    if (x <= mid)
+        modify(ls, l, mid, x, v);
+    else
+        modify(rs, mid + 1, r, x, v);
+    update(u);
+}
+tree find (int u, int l, int r, int x, int y)
+{
+    if (x > y) return (tree){0, 0, 0}; 
+    if (x <= l && r <= y)
+        return t[u];
+    tree ret, lret, rret;
+    ret.lx = ret.rx = ret.sum = 0;
+    rret = ret;
+    int mid = l + r >> 1;
+    if (x <= mid)
+        ret = find(ls, l, mid, x, y);
+    if (mid < y)
+        rret = find(rs, mid + 1, r, x, y);
+    ret.lx = std::max(ret.lx, ret.sum + rret.lx);
+    ret.rx = std::max(rret.rx, rret.sum + ret.rx);
+    ret.sum += rret.sum;
+    return ret;
+}
+std::priority_queue<pi> q;
+int last, sta[N], top, ans[N];
 int main ()
 {
-    int T = read();
-    while (T--)
-    {
+    n = read();
+    fo (i, 1, n) a[i] = read();
 
+    build(1, 1, n);
+    fo (i, 1, n) q.push(mp(a[i], i));
+    q.push(mp(0, n + 1));
+    last = q.top().F;
+    while (q.size())
+    {
+        pi now = q.top();
+        q.pop();
+        if (now.F != last)
+        {
+            fo (i, 1, top)
+                modify(1, 1, n, sta[i], 1);
+            fo (i, 1, top)
+                ans[sta[i]] = std::max(ans[sta[i]], (find(1, 1, n, sta[i] + 1, n).lx + find(1, 1, n, 1, sta[i] - 1).rx + 1) / 2);
+            top = 0;
+            last = now.F;
+        }
+        sta[++top] = now.S;
+        ans[now.S] = (find(1, 1, n, now.S + 1, n).lx + find(1, 1, n, 1, now.S - 1).rx + 1) / 2;
     }
+    
+    build(1, 1, n);
+    top = 0;
+    fo (i, 1, n) q.push(mp(-a[i], i));
+    q.push(mp(-n-1, n + 1));
+    last = q.top().F;
+    while (q.size())
+    {
+        pi now = q.top();
+        q.pop();
+        if (now.F != last)
+        {
+            fo (i, 1, top)
+                modify(1, 1, n, sta[i], 1);
+            fo (i, 1, top)
+                ans[sta[i]] = std::max(ans[sta[i]], (find(1, 1, n, sta[i] + 1, n).lx + find(1, 1, n, 1, sta[i] - 1).rx) / 2);
+            top = 0;
+            last = now.F;
+        }
+        sta[++top] = now.S;
+        ans[now.S] = std::max(ans[now.S], (find(1, 1, n, now.S + 1, n).lx + find(1, 1, n, 1, now.S - 1).rx) / 2);
+    }
+    fo (i, 1, n) printf("%d ", ans[i]);
     return 0;
 }
